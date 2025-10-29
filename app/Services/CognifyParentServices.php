91be71ca337e19dd8168e0fca569d3a39a388970
Shimaa Mohamed <shimaa0mohamed19@gmail.com>
2025-10-29 19:02:44 +0300

    VerifyPhoneEmail

--- app/Services/CognifyParentServices.php
+++ app/Services/CognifyParentServices.php
@@ -41,8 +41,6 @@ class CognifyParentServices
 
         return apiResponse(true, [], __('messages.otp_sent'));
     }
-
-
     public function verifyOTP(array $data)
     {
         $phone = $data['phone'];
@@ -93,62 +91,6 @@ class CognifyParentServices
             'parent' => $parent,
         ], __('messages.register_success'));
     }
-
-    // public function ParentRegister(array $array)
-    // {
-    //     $otp = rand(100000, 999999);
-    //     $cacheKey = 'parent_register_' . $array['phone'];
-    //     Cache::put($cacheKey, [
-    //         'data' => $array,
-    //         'otp' => $otp,
-    //     ], now()->addMinutes(10));
-
-    //     $key=app(SMSService::class)->sendSMS($array['phone'], "Your Cognify verification code is: {$otp}");
-    //     if (!$key) {
-    //         return false;
-    //     }
-    //     return  true;
-
-    // }
-
-    // public function verifyOTP(array $data)
-    // {
-    //     $phone = $data['phone'];
-    //     $enteredOtp = $data['otp'];
-
-    //     $cacheKey = 'parent_register_' . $phone;
-    //     $cached = Cache::get($cacheKey);
-
-    //     if (!$cached) {
-    //         return apiResponse(false, [], __('messages.otp_expired'));
-    //     }
-
-    //     if ($cached['otp'] != $enteredOtp) {
-    //         return apiResponse(false, [], __('messages.invalid_otp'));
-    //     }
-
-    //     $array = $cached['data'];
-    //     $parent = CognifyParent::create([
-    //         'name'        => $array['name'] ?? null,
-    //         'email'       => $array['email'],
-    //         'phone'       => $array['phone'] ?? null,
-    //         'address'     => $array['address'] ?? null,
-    //         'governorate' => $array['governorate'] ?? null,
-    //         'password'    => $array['password'],
-    //         'role'        => $array['role'] ?? 'parent',
-    //         'step'        => 2
-    //     ]);
-
-    //     Cache::forget($cacheKey);
-    //     $token = $parent->createToken('parent_token')->plainTextToken;
-
-    //     return apiResponse(true, [
-    //         'token' => $token,
-    //         'parent' => $parent,
-    //     ], __('messages.register_success'));
-    // }
-
-
 
     static function loginParent(array $array)
     {
